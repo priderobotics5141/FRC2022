398fe358e6c0f9409ec8ec6bb9bb8da67a3d4d4c
Winnetonka <priderobotics5141@gmail.com>
2022-03-09 16:15:01 -0600

    climb outline

--- src/main/java/frc/robot/Climb.java
+++ src/main/java/frc/robot/Climb.java
@@ -7,13 +7,13 @@ import edu.wpi.first.wpilibj.Servo;
 
 public class Climb {
 
-    TalonFX one = new TalonFX(8);
-    TalonFX two = new TalonFX(9);
+    TalonFX fxOne = new TalonFX(8);
+    TalonFX fxTwo = new TalonFX(9);
 
     Servo three = new Servo(4);
     Servo four = new Servo(5);
 
-    public Climb() {
+    public Climb() { //init
         three.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
         four.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
 
@@ -21,21 +21,38 @@ public class Climb {
     
 
     public void climb() {
-        three.setSpeed(1.0); // to open
-        three.setSpeed(-1.0); // to close
-
         extendMotors();
         retractMotors();
-
+        servoBack();
+        extendMotors();
+        servoMid();
+        retractMotors();
+        servoBack();
+        extendMotors();
+        retractMotors();
+        servoBack();
+        extendMotors();
+        servoMid();
+        retractMotors();
 
     }
 
     public void extendMotors() {
-        one.set(TalonFXControlMode.Position, 100);
-        two.set(TalonFXControlMode.Position, 100);
+        fxOne.set(TalonFXControlMode.Position, 100);
+        fxTwo.set(TalonFXControlMode.Position, 100);
     }
     public void retractMotors() {
-        one.set(TalonFXControlMode.Position, 0);
-        two.set(TalonFXControlMode.Position, 0);
+        fxOne.set(TalonFXControlMode.Position, 0);
+        fxTwo.set(TalonFXControlMode.Position, 0);
     }
+
+    public void servoBack() {
+        three.setSpeed(-1.0);
+        four.setSpeed(-1.0);
+    }
+    public void servoMid() {
+        three.setSpeed(1.0);
+        four.setSpeed(1.0);
+    }
+
 }
